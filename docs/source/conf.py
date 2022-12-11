572428f7ef52c62122b50eda3a86d6468e3f9324
Spooky <73013941+microbullet@users.noreply.github.com>
2022-12-11 15:33:04 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,10 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_static_path = ['_static']
+
+html_css_files = ["custom.css"]
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
